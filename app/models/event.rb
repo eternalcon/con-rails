45adf45a642790308f2a2253adcd49e3dcef098f
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2016-12-28 11:30:54 +1100

    removed associations to games and game blocks from events model for now - get event registration done before worrying about anything else

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   has_many :registrations
   has_many :participants, :through => :registrations
-  has_many :game_blocks, :order => "order_value ASC" 
-  has_many :games, :order => "game_title ASC" 
+#  has_many :game_blocks, :order => "order_value ASC" 
+#  has_many :games, :order => "game_title ASC" 
 
   has_and_belongs_to_many :prices
   validates_presence_of :name, :startdate, :enddate
@@ -13,5 +13,4 @@ class Event < ApplicationRecord
   def self.active_event
     Event.find(:first, :conditions => ['active=?',true])
   end
-end
 end
