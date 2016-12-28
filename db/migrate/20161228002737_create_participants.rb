e164799977b00df99829a70262523aae565ee271
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2016-12-28 11:29:04 +1100

    added participants model

---
+++ db/migrate/20161228002737_create_participants.rb
@@ -0,0 +1,17 @@
+class CreateParticipants < ActiveRecord::Migration[5.0]
+  def change
+    create_table :participants do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :street
+      t.string :postcode
+      t.string :city
+      t.string :country
+      t.string :notes
+      t.string :newsletter
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/participants.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :participant do
+    first_name "MyString"
+    last_name "MyString"
+    email "MyString"
+    street "MyString"
+    postcode "MyString"
+    city "MyString"
+    country "MyString"
+    notes "MyString"
+    newsletter "MyString"
+  end
+end
