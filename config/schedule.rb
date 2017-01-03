4fcdbca92b1a40db92fb5e8b17b138d649db0413
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-01-03 15:07:58 +1100

    added schedule for automatic dumping of production data and importing to staging as well as required rake tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,27 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
+set :production_dump_file, "/tmp/production_dump_daily.sql.gz"
+#set :remote_dump_file, "dumper@app.mydomain.com:/tmp/production_dump_daily.sql.gz"
+set :staging_dump_file, "/tmp/production_dump_daily.sql.gz"
+
+if environment == "production" || environment == "staging"
+  # normal crons goes here
+end
+
+if environment == "production"
+  # Export daily dump
+  every :day, at: "12:00am" do
+    command "cd #{path} && #{environment_variable}=#{environment} FILE=#{production_dump_file} bin/rake dump:export"
+  end
+end
+
+if environment == "staging"
+  # Import daily dump
+  every :day, at: "12:30am" do
+    command "cd #{path} && #{environment_variable}=#{environment} FILE=#{staging_dump_file} bin/rake dump:retrieve dump:barrier maintenance:enable db:drop db:create dump:import db:migrate maintenance:restart maintenance:disable"
+  end
+end
 
 # Example:
 #
