73e50b4a64f34f619b806ec7fe5911be3ef81a39
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-08-20 19:46:36 +0200

    changed cucumber environment to require rspec and capybara for better test options

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,8 @@
 # files.
 
 require 'cucumber/rails'
+require 'rspec/expectations'
+require 'capybara/cucumber'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
