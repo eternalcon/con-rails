84b0941d662df05ee5493b7a80ad9b69d3490685
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-08-20 19:52:47 +0200

    some explanations for the failing routes setup for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # set something as root to: for now, since active_admin requires some sort of root.
+  # to be replaced as soon as a true root is present in the app.
   root to: "home#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
