c9586d41bf7c15b26d42f5e284c92b849ce20f28
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-08-20 10:58:21 +0200

    first cucumber features for events administration

---
+++ features/step_definitions/events_admin_steps.rb
@@ -0,0 +1,8 @@
+When(/^I access the event admin panel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will have access to the "([^"]*)" option$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
