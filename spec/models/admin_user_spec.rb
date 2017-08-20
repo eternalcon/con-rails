6645e11022d6dbc87823bafb97efcfc9f3887f4e
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-08-20 20:37:47 +0200

    generate admin_user factory for test data and model spec to check for valid factory

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
-
+# Since we use devise for admin user authentication and authorization,
+# we don't need to test the model properly since devise should already be 
+# fully tested. We simply test wether our factory for generating test data
+# is set up properly.
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    build(:admin_user).should be_valid
+  end
 end
