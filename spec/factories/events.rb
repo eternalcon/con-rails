0c8782d92d7b42d93bb2bf8f45359f6791c5027a
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-08-29 15:47:43 +0900

    make events factory a bit more dynamic, setting start and end date dynamically

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :event do
     name "MyString"
-    start_date "2017-08-29"
-    end_date "2017-08-29"
+    start_date { Time.now + 30.days }
+    end_date { Time.now + 33.days }
   end
 end
