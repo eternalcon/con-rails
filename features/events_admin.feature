690836dc84dcf76e28ca7e843620d9cb9af0dfba
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-08-29 10:23:03 +0900

    cucumber feature extended to test that only admins can access the admin panel - next: get the events admin panel available

--- features/events_admin.feature
+++ features/events_admin.feature
@@ -3,6 +3,12 @@ Feature: Events Admin
   When I visit the Administration Panel,
   I want to be able to administrate events.
   
-  Scenario: Administrators will have access to the events admin panel
+  Scenario: Only Administrators will have access to the events admin panel
     When I access the admin panel as administrator
+    Given I am not logged in as admin user
+    Then I should be presented with a login screen
+    And not have access to the events admin panel
+    Given I am logged in as admin user
     Then I will have access to the events admin panel
+    
+    
--- features/step_definitions/events_admin_steps.rb
+++ features/step_definitions/events_admin_steps.rb
@@ -1,6 +1,27 @@
-When(/^I access the admin panel$/) do
+When(/^I access the admin panel as administrator$/) do
   visit '/admin'
+  
 end
+
+Given(/^I am not logged in as admin user$/) do
+  visit '/admin'
+  User = nil  
+end
+
+Then(/^I should be presented with a login screen$/) do
+  expect(page).to have_field 'Password'
+  expect(page).to have_button 'Login'
+end
+
+Then(/^not have access to the events admin panel$/) do
+  visit '/admin/events'
+  expect(page).to not_have_content
+end
+
+Given(/^I am logged in as admin user$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 
 Then(/^I will have access to the events admin panel$/) do
   pending # Write code here that turns the phrase above into concrete actions
