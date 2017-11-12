d4d201473983c5c5bd7f383fceb54bd9055fb908
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-11-12 16:55:06 +0100

    some more factory girl to factory bot

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module ConRails
         :routing_specs => true,
         :controller_specs => true,
         :request_specs => true
-      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.fixture_replacement :factory_bot, :dir => "spec/factories"
     end
   end
 end
