6c3c34435cfb6de3b8ce932549dc25c004b963c9
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-11-12 17:42:29 +0100

    made all fields of the events model required and added some model validation including specs

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+
+  validates :name, :start_date, :end_date, presence: true
+  validate :end_date_must_be_after_start_date
+    
+  def end_date_must_be_after_start_date
+    return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check the correct order of dates...
+    if end_date.present? && end_date <= start_date
+      errors.add(:end_date, "can't be the same or earlier than start date")
+    end
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Event, type: :model do
   it "has a valid factory" do
     expect(build(:event)).to be_valid
   end
+  it "is invalid without a name" do 
+    expect(build(:event, name: nil)).to be_invalid
+  end
+  it "is invalid without a start date" do 
+    expect(build(:event, start_date: nil, end_date: Faker::Time.forward(360, :morning))).to be_invalid
+  end
+  it "is invalid without a end date" do 
+    expect(build(:event, end_date: nil)).to be_invalid
+  end
+  it "is invalid if end date is the same as start date or before start date" do
+    expect(build(:event, start_date: Time.now, end_date: Time.now)).to be_invalid
+    expect(build(:event, start_date: Time.now, end_date: Time.now - 3.days )).to be_invalid
+    #expect(build(:event, end_date: @event.start_date - 3.days)).to be_invalid
+  end
+  
 end
