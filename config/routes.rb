6d9e00835b2b85e81239007e7978ee038225d8b3
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-11-12 17:02:49 +0100

    basic i18n setup for german and english using url 'subdirectories' to pass locale

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: "events#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :events
+  
+  scope "(:locale)", locale: /en|de/ do
+    resources :events, only: [ :index ]  
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
