df7613a6807ef6277cd751ea0b9ad38e9dfe6b55
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-11-19 19:17:26 +0100

    changed registrations table to event_registrations table due to some conflicts with devise routes

--- db/migrate/20171119150423_create_event_registrations.rb
+++ db/migrate/20171119150423_create_event_registrations.rb
@@ -1,4 +1,4 @@
-class CreateRegistrations < ActiveRecord::Migration[5.1]
+class CreateEventRegistrations < ActiveRecord::Migration[5.1]
   def change
     create_table :registrations do |t|
       t.references :event, foreign_key: true
