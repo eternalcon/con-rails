4f9e5773324c3cbb18e0d01ac7e1783921cb5aa0
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-11-26 14:58:54 +0100

    Created a set_status method on the Events model to automatically set the event status based on some data. Still need some cases for certain status values. Tests stubbed out to check correct status is set based on available data.
    Needs work on some parts commented in the code.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,39 @@
 class Event < ApplicationRecord
+  attr_accessor :status
   has_many :event_registrations
   has_many :participants, through: :event_registrations
   validates :name, :start_date, :end_date, presence: true
   validate :end_date_must_be_after_start_date
-    
-  def end_date_must_be_after_start_date
-    return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check the correct order of dates...
-    if end_date.present? && end_date <= start_date
-      errors.add(:end_date, "can't be the same or earlier than start date")
-    end
+  after_validation do |event|
+    set_status  
   end
+    def set_status
+      puts self.start_date
+      puts self.end_date
+      return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check the correct order of dates...
+
+      if (Time.now.utc.to_date.year < start_date.year) # The event happens in a future year - active event but registration disabled
+        self.status = "pending"
+      elsif (Time.now.utc.to_date.year == start_date.year) && (Time.now.utc.to_date < start_date) # The event happens this year, but hasn't happened yet - Registration open
+        self.status = "active"
+      #elsif Time.now.utc.to_date >= :start_date # We're in the same year as the event, but the event is too close to allow online registrations 
+      #  # TODO: figure out how close to the event is too close - one or two weeks in advance sounds reasonable. Will most likely not bee needed anyways since we book out too fast.
+      #  self.status = "waiting"
+      elsif Time.now.utc.to_date > end_date # The event is in the past
+        self.status = "past"
+  #    elsif # TODO: Add a case for active events which are booked out for online registration.
+  #      self.status = "full"
+      elsif Time.now.utc.to_date > start_date && Time.now.utc.to_date < end_date # Today is between start and end date - the event is currently on.
+        self.status = "running"
+      else
+        self.status = nil
+      end
+    end
+
+    def end_date_must_be_after_start_date
+      return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check the correct order of dates...
+      if end_date.present? && end_date <= start_date
+        errors.add(:end_date, "can't be the same or earlier than start date")
+      end
+    end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -16,7 +16,47 @@ RSpec.describe Event, type: :model do
   it "is invalid if end date is the same as start date or before start date" do
     expect(build(:event, start_date: Time.now, end_date: Time.now)).to be_invalid
     expect(build(:event, start_date: Time.now, end_date: Time.now - 3.days )).to be_invalid
-    #expect(build(:event, end_date: @event.start_date - 3.days)).to be_invalid
   end
   
+  # Test for event status switch based on date
+  it "is pending if it happens in a future year" do
+    event = create(:event, start_date: Time.now + 2.years, end_date: Time.now + 2.years + 3.days)
+    expect(event.status).to eq "pending"
+  end
+  
+  it "is active if it happens this year and far enough in the future" do
+    # FIXME: We have a testing problem here, especially in comparison with the next case. If we run tests towards the end of the year, i.e. before the registration is supposed to start,
+    # the distance needed from Time.now to make the event happen in the future might already result in the event being assigned a start date past new years, making the event pending instead of active.
+    # It may be impossible to generate an event which still falls in the "active" category towards the year's end.
+
+    event = create(:event, start_date: Time.now + 2.days, end_date: Time.now + 5.days)
+    expect(event.status).to eq "active"
+  end
+#  it "is in waiting stage if it happens this year but is too close to allow online registration" do
+#    event = create(:event, start_date: Time.now + 2.years, end_date: Time.now + 2.years + 3.days)
+#    expect(event.status).to eq "waiting"
+#  end
+
+  it "is past if the end_date is earlier than Time.now" do
+    event = create(:event, start_date: Time.now - 5.days, end_date: Time.now - 3.days)
+    expect(event.status).to eq "past"
+  end
+
+#  it "is full if the maximum number of spaces has been booked" do
+#    #event = create(:event, start_date: Time.now - 5.days, end_date: Time.now - 3.days)
+#    expect(event.status).to eq "full"
+#  end
+
+  it "is running if today is between start_date and end_date" do
+    event = create(:event, start_date: Time.now - 1.day, end_date: Time.now + 2.days)
+    expect(event.status).to eq "running"
+  end
+
+#    when Time.now.utc.to_date > :end_date # The event is in the past
+#      :status = "past"
+#    when # TODO: Add a case for active events which are booked out for online registration.
+#      :status = "full"
+#    when Time.now.utc.to_date > :start_date && Time.now.utc.to_date < :end_date # Today is between start and end date - the event is currently on.
+#      :status = "running"
+#   end
 end
