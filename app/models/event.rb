c1f40b6d6fd0938c891c3dc3667ce4eb8aa1386f
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-12-24 16:34:26 +0100

    added status column to events and made status to be set automatically on each find action

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  attr_accessor :status
+  #attr_accessor :status
   has_many :event_registrations
   has_many :participants, through: :event_registrations
   validates :name, :start_date, :end_date, presence: true
@@ -7,29 +7,29 @@ class Event < ApplicationRecord
   after_find do |event|
     set_status  
   end
+   
     def set_status
-      puts self.start_date
-      puts self.end_date
       return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check the correct order of dates...
 
       if (Time.now.utc.to_date.year < start_date.year) # The event happens in a future year - active event but registration disabled
-        self.status = "pending"
+        self.update(status: 'pending')
       elsif (Time.now.utc.to_date.year == start_date.year) && (Time.now.utc.to_date < start_date) # The event happens this year, but hasn't happened yet - Registration open
-        self.status = "active"
+        self.update(status: 'active')
       #elsif Time.now.utc.to_date >= :start_date # We're in the same year as the event, but the event is too close to allow online registrations 
       #  # TODO: figure out how close to the event is too close - one or two weeks in advance sounds reasonable. Will most likely not bee needed anyways since we book out too fast.
-      #  self.status = "waiting"
+      #  self.status = 'waiting'
       elsif Time.now.utc.to_date > end_date # The event is in the past
-        self.status = "past"
+        self.update(status: 'past')
   #    elsif # TODO: Add a case for active events which are booked out for online registration.
-  #      self.status = "full"
+  #      self.status = 'full'
       elsif Time.now.utc.to_date > start_date && Time.now.utc.to_date < end_date # Today is between start and end date - the event is currently on.
-        self.status = "running"
+        self.update(status: 'running')
       else
-        self.status = nil
+      #  self.update(status: nil)
       end
+      #self.save!
     end
-
+    
     def end_date_must_be_after_start_date
       return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check the correct order of dates...
       if end_date.present? && end_date <= start_date
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119150423) do
+ActiveRecord::Schema.define(version: 20171224122758) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20171119150423) do
     t.decimal "full_price", precision: 10
     t.decimal "day_guest", precision: 10
     t.decimal "child", precision: 10
+    t.string "status"
   end
 
   create_table "participants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
