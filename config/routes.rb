444254b7b14252e79069ab529f5a4b68a2bbc027
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-12-24 16:36:04 +0100

    changes to the routes to reflect nesting of events and event_registrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,20 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
     root to: "events#index"
     devise_for :users
-    resources :events, only: [ :index ]
-    resources :participants do
-      resources :events
-    end
-    resources :events do
-      resources :participants
-    end
-    resources :event_registrations do
-      resources :events
-      resources :participants
+    #resources :events
+    #resources :participants, :events
+#    resources :participants do
+#      resources :events
+#    end
+#    resources :events do
+#      resources :participants
+#    end
+    #resources :event_registrations do
+    #  resources :events
+    #  resources :participants
+    #end
+    resources :events, only: [ :index ] do
+      resources :event_registrations, :participants
     end
   end
 
