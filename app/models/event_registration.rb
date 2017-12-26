70e40b109a63caeff29e37fbd14eeae7711c79e7
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-12-27 00:37:47 +0100

    added cocoon gem for handling of dynamic nested attributes form and converted our forms over. Frontend seems to work correctly now

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -2,5 +2,5 @@ class EventRegistration < ApplicationRecord
   belongs_to :event
   belongs_to :user
   has_and_belongs_to_many :participants
-  accepts_nested_attributes_for :participants
+  accepts_nested_attributes_for :participants, reject_if: :all_blank, allow_destroy: true
 end
