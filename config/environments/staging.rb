cdd797527be5e85d8c6f356431cda4719253399e
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-12-29 14:27:01 +0100

    devise should also use the correct host in staging...

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "con_rails_#{Rails.env}"
   
   ########## Action Mailer Configuration for Production ##########
-  config.action_mailer.default_url_options = { host: 'eternal-con.de' } # needed for devise
+  config.action_mailer.default_url_options = { host: 'staging.eternal-con.de' } # needed for devise
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
