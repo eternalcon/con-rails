be3ca1ef4be0061b206e25e05d14ba24beaf73ce
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-12-29 13:26:42 +0100

    changed db:seeds file to create activeadmin user in staging and production by pulling data from secrets.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: Rails.application.secrets[:activeadmin][:admin_email], password: Rails.application.secrets[:activeadmin][:admin_password], password_confirmation: Rails.application.secrets[:activeadmin][:admin_confirm]) if Rails.env.staging? || Rails.env.production?
