ccb5f24f22d3f79f2ce6d1fd5c335cf49820b5c8
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2017-12-29 10:15:52 +0100

    delayed job for registration_confirm mail activated - mails will get sent

--- app/controllers/event_registrations_controller.rb
+++ app/controllers/event_registrations_controller.rb
@@ -26,6 +26,7 @@ class EventRegistrationsController <  ApplicationController
     #@event_registration.participants.create
     if @event_registration.save
     redirect_to event_registration_path(@event_registration)
+    EventRegistrationMailer.registration_confirm(@event_registration).deliver_later
     else
       render 'new'
     end
