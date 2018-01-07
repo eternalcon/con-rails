8856d8f8922d1164ab0f2a434bc239a94ab574de
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-01-07 13:18:04 +0100

    made the rescue from CanCan Access denied work with json and js requests as well.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,10 @@ class ApplicationController < ActionController::Base
   end
   
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = exception.message
-    redirect_to root_url
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.js   { head :forbidden, content_type: 'text/html' }
+    end
   end
 end
