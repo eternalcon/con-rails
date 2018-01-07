7ef967d34355d93b8c7424f7fb14ba0f2ee3f827
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-01-07 19:16:08 +0100

    added mailer for payment confirmation including views

--- app/mailers/event_registration_mailer.rb
+++ app/mailers/event_registration_mailer.rb
@@ -11,4 +11,9 @@ class EventRegistrationMailer < ApplicationMailer
     @event_registration = event_registration
     mail(to: ['andreas_pittelkow@web.de', 'info@eternal-con.de', '1812raevsky@gmail.com'], subject: '[Eternal Con Registration ] - neue Anmeldung für' + @event_registration.event.name)
   end
+  
+  def payment_confirm(event_registration)
+    @event_registration = event_registration
+    mail(to: @event_registration.user.email, subject: default_i18n_subject(event: @event_registration.event.name) + "Payment Confirmed!")
+  end
 end
