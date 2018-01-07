2676b1e51c1fb0032c2ec0560eb5f652a024a793
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-01-07 12:37:59 +0100

    some comments on the roles model as to how roles should look like

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,5 @@
 class Role < ApplicationRecord
   has_and_belongs_to_many :users
+  # Roles are supposed to be stored in the database as CamelCased names.
+  # TODO: add a validation for role name format.
 end
