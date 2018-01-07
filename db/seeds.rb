8f69463b62775db5b32731236ce1c8a5122dc337
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-01-07 16:40:24 +0100

    added the creation of an admin role to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Create the Admin role to be used by CanCanCan - Add more roles if needed.
+Role.create(name: 'Admin')
