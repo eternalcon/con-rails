6eec6e3eddaaac0bfec937017b6a327cc90f9aab
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-01-07 15:38:46 +0100

    moved the admin event_registrations controller stuff and views out of admin namespace

--- app/controllers/event_registrations_controller.rb
+++ app/controllers/event_registrations_controller.rb
@@ -1,6 +1,8 @@
 class EventRegistrationsController <  ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
+  respond_to :html, :xls
+
   # TODO: We need to handle existing participants in the whole thing for future events.
   # TODO: The first registered participant needs to be assigned to the user if it is not there already (or any other way of having the current_user.participant be set properly.
   # TODO: We need to at least "recycle" existing participants when a record matching the entered data already exists in the database - i.e. only create the link between event_registration and participant
@@ -9,9 +11,17 @@ class EventRegistrationsController <  ApplicationController
   # TODO: Not quite related to this controller, but it would be cool to add an option for users to claim "their" participant - i.e. if somebody else registered my name last year and now I want to do it myself,
   # TODO: have the existing participant linked to my user.
   
+  def index
+    @event = Event.find(params[:event_id])
+    @event_registrations = @event.event_registrations
+    respond_with(@event_registrations) do |format|
+      format.html
+      format.xls # { send_data to_csv(@admin_event_registrations) }
+    end
+  end
+
   def new
     if Event.find_by(status: 'active') != nil
-   
       @event_registration = EventRegistration.new do |r|
         r.event_id = Event.find_by(status: 'active').id
         r.user_id = current_user.id
@@ -26,6 +36,14 @@ class EventRegistrationsController <  ApplicationController
     end    
   end
   
+  def show
+    respond_with(@event_registration)
+  end
+
+  def edit
+  
+  end
+
   def create
     @event_registration = EventRegistration.new(event_registration_params)
     @event_registration.save
@@ -39,8 +57,15 @@ class EventRegistrationsController <  ApplicationController
     end
   end
   
-  def show
+  
+  #def update
+  #  @event_registration.update(event_registration_params)
+  #  respond_with(@event_registration)
+  #end
 
+  def destroy
+    @event_registration.destroy
+    respond_with(@event_registration)
   end
 
   private
