a7aab3b399f91cf7b0833d7b7513c731e00f60df
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-01-14 14:52:11 +0100

    added model for freeform module

---
+++ app/models/freeform.rb
@@ -0,0 +1,3 @@
+class Freeform < ApplicationRecord
+  belongs_to :event
+end
---
+++ db/migrate/20180114135125_create_freeforms.rb
@@ -0,0 +1,16 @@
+class CreateFreeforms < ActiveRecord::Migration[5.1]
+  def change
+    create_table :freeforms do |t|
+      t.string :name
+      t.text :description_de
+      t.text :description_en
+      t.string :language
+      t.integer :min_age
+      t.integer :max_age
+      t.integer :max_participants
+      t.references :event, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
