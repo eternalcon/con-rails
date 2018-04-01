4a341a25025b81f6819cea1714e5ef2d39856409
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-01 13:44:58 +0200

    removed unused before_filter from events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
     before_action :authenticate_user!, :except => [ :home ]
 #    before_action :set_admin_event, only: [:show, :edit, :update, :destroy]
-    before_action :check_for_event_full, only: [:new, :create]
+#    before_action :check_for_event_full, only: [:new, :create]
     load_and_authorize_resource
     respond_to :html
 
