b85ea1de3813db3c8eebed1ea3431026ef4d5939
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-01 22:31:59 +0200

    added routes for ff_registrations as direct route

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,9 @@ Rails.application.routes.draw do
     resources :event_registrations do
       resources :participants, :users
     end
+    resources :ff_registrations do
+      resources :participants, :users
+    end
     resources :freeforms
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
