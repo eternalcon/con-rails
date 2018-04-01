fcf7303c22dfcfa1c6a6c39ab94c13abd2c5dd28
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-01 13:12:11 +0200

    added author email to freeforms so registrations can be sent directly to the author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303122422) do
+ActiveRecord::Schema.define(version: 20180401111007) do
 
   create_table "delayed_jobs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "priority", default: 0, null: false
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20180303122422) do
     t.bigint "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author_email"
     t.index ["event_id"], name: "index_freeforms_on_event_id"
   end
 
