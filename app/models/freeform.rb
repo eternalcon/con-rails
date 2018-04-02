04c1b56dd90932f05e1823160fc6719c30266192
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-02 09:16:33 +0200

    add set_status method on freeform model to set status to full if max_participants reached. TODO: Prevent registrations of more people than fit the game

--- app/models/freeform.rb
+++ app/models/freeform.rb
@@ -1,4 +1,16 @@
 class Freeform < ApplicationRecord
   belongs_to :event
   has_many :ff_registrations
+  
+  def set_status
+      return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check anything else...
+      return if self.status == "inactive" # If we set the status to inactive manually, we don't want it to be changed automatically...
+      if self.participants.size > self.max_participants
+        self.status = 'full'
+      else
+        self.status = 'active'
+      end
+      self.save
+    end
+    
 end
