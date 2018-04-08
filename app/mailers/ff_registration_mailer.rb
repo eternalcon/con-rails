e1889f341ee36d39ff993bc5342670ab1ca6c50f
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-08 20:51:28 +0200

    added default from address to ff_registrations_mailer

--- app/mailers/ff_registration_mailer.rb
+++ app/mailers/ff_registration_mailer.rb
@@ -1,4 +1,5 @@
 class FfRegistrationMailer < ApplicationMailer
+  default from: 'info@eternal-con.de'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
