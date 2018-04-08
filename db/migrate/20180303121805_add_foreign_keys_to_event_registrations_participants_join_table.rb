28f7abdea9d52965b291b91e9642ea262b05fe77
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-08 21:46:28 +0200

    remove foreign key migration for now as it is causing some strange problems failing to migrate in production while everything went fine in staging

--- db/migrate/20180303121805_add_foreign_keys_to_event_registrations_participants_join_table.rb
+++
@@ -1,8 +0,0 @@
-class AddForeignKeysToEventRegistrationsParticipantsJoinTable < ActiveRecord::Migration[5.1]
-  def change
-    add_foreign_key :event_registrations_participants, :event_registrations
-  end
-  def change
-    add_foreign_key :event_registrations_participants, :participants
-  end
-end
