ad6f0e03256867bb31488b65c6c098e0176fc087
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-08 09:34:01 +0200

    made freeform model actually set status on find

--- app/models/freeform.rb
+++ app/models/freeform.rb
@@ -1,11 +1,16 @@
 class Freeform < ApplicationRecord
   belongs_to :event
   has_many :ff_registrations
+  has_many :participants, through: :ff_registrations
+  
+  after_find do |freeform|
+    set_status  
+  end
   
   def set_status
       return unless errors.blank? # if we already hit an error when checking for the presence of all required attributes, we don't need to check anything else...
       return if self.status == "inactive" # If we set the status to inactive manually, we don't want it to be changed automatically...
-      if self.participants.size > self.max_participants
+      if self.participants.size >= self.max_participants
         self.status = 'full'
       else
         self.status = 'active'
