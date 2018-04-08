18abca1cd6b45cfdd1becef9086c55b97963e14d
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-04-08 21:43:32 +0200

    fixes to migration adding foreign keys

--- db/migrate/20180303121805_add_foreign_keys_to_event_registrations_participants_join_table.rb
+++ db/migrate/20180303121805_add_foreign_keys_to_event_registrations_participants_join_table.rb
@@ -1,6 +1,8 @@
 class AddForeignKeysToEventRegistrationsParticipantsJoinTable < ActiveRecord::Migration[5.1]
   def change
     add_foreign_key :event_registrations_participants, :event_registrations
+  end
+  def change
     add_foreign_key :event_registrations_participants, :participants
   end
 end
