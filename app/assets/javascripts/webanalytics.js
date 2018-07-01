62a70ef2fb51949062e27e3bed89ef6368eca893
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-07-01 16:40:00 +0200

    changed matomoto javascript to not track users without javascript enabled

--- app/assets/javascripts/webanalytics.js
+++ app/assets/javascripts/webanalytics.js
@@ -3,7 +3,6 @@
   var _paq = _paq || [];
   // require user consent before processing data
   _paq.push(['requireConsent']);
-  _paq.push([‘trackPageview']);
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
   _paq.push(["setCookieDomain", "*.eternal-con.de"]);
@@ -18,5 +17,4 @@
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//stats.eternal-con.de/piwik.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Matomo Code -->
