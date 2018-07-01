276be7f302c6d6cfe34f26338a9d053b99f81b16
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-07-01 16:27:02 +0200

    reactivated matomoto web analytics with data protection options set

---
+++ app/assets/javascripts/webanalytics.js
@@ -0,0 +1,22 @@
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  // require user consent before processing data
+  _paq.push(['requireConsent']);
+  _paq.push([‘trackPageview']);
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.eternal-con.de"]);
+  _paq.push(["setDomains", ["*.eternal-con.de"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.eternal-con.de/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//stats.eternal-con.de/piwik.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
