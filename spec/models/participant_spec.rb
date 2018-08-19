32e4bfa77783b541eb87d6d66d585a1923c5762e
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-08-19 18:45:47 +0200

    basic model spec for participants

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Participant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.build(:participant)).to be_valid
+  end
+  it "is invalid without a first name" do
+    expect(FactoryBot.build(:participant, first_name: nil)).to be_invalid
+  end
+  it "is invalid without a last name" do 
+    expect(FactoryBot.build(:participant, last_name: nil)).to be_invalid
+  end
+  it "is invalid without a country" do 
+    expect(FactoryBot.build(:participant, country: nil)).to be_invalid
+  end
+  it "is invalid without a birth date" do 
+    expect(FactoryBot.build(:participant, birthdate: nil)).to be_invalid
+  end
+  let(:participant_attr) { FactoryBot.attributes_for(:participant) }
+  it "can not be created if a participant with the same name and birthdate already exists" do
+    participant1 = Participant.create!(participant_attr)
+    expect(participant2 = Participant.create(participant_attr)).to be_invalid
+  end
+    
 end
