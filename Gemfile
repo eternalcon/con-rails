89ff72d25cf620f913b732724968840ca8da4c9d
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2018-08-19 09:52:54 +0200

    properly activated active_storage to make app start again

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+# for railties app_generator_test
+gem "bootsnap", ">= 1.1.0", require: false
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,10 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Store uploaded files on the local file system in a temporary directory
+  config.active_storage.service = :development
+
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
