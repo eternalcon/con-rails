6b82471085d7b3c7699cc74777a6794ce8d5a9a8
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2019-05-12 22:44:19 +0200

    still some errors left...

--- app/assets/javascripts/webanalytics.js
+++ app/assets/javascripts/webanalytics.js
@@ -1,7 +1,4 @@
-// Matomo Web Analytics
-<script type="text/javascript">
   var _paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
   _paq.push(["setCookieDomain", "*.eternal-con.de"]);
   _paq.push(["setDomains", ["*.eternal-con.de"]]);
@@ -14,6 +11,4 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
-<noscript><p><img src="//stats.eternal-con.de/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-//<!-- End Matomo Code -->
+
