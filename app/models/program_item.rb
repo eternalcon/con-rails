4ae99d415cca0bbdaff593906b5eb883456ea804
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2019-05-12 11:11:42 +0200

    Set relation on program_items to belongs_to with events having many program items. Added route to events/program_items

---
+++ app/models/program_item.rb
@@ -0,0 +1,3 @@
+class ProgramItem < ApplicationRecord
+  belongs_to :event
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
     resources :events do
       collection do
         get 'home'
-      end  
+      end
+      resources :program_items  
       resources :event_registrations do
         member do
           post 'mark_as_payed'
@@ -34,6 +35,7 @@ Rails.application.routes.draw do
       resources :participants, :users
     end
     resources :freeforms
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
